e80d44134745fb8618c7096ffb2cd14c96aaaf28
ClementDucloy <DUCLOY1.C@epid-vauban.fr>
2026-09-08 12:31:43 +0200

    first commit  08/09

---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+console.log("Loading Index");
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+	let slides = document.getElementsByClassName('slides'); 
+	let dots = document.getElementsByClassName('dot');
+	
+	console.log(slides);
+	console.log(dots);
+	
+	if(n > slides.length) {slideIndex = 1}
+		
+	if(n < 1 ) {slideIndex = slides.length}
+	
+	for(let i = 0; i < dots.length; i++) {
+		slides[i].style.display = "none";
+	}
+	
+	for(let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('active');
+	}
+	
+	slides[slideIndex - 1].style.display = 'block';
+	
+	dots[slideIndex - 1].classList.add('active');
+ }
+ 
+ function plusSlides(n) {
+	 showSlides(slideIndex += n);
+ }
+ 
+ function currentSlide(n) {
+	 showSlides(slideIndex = n);
+ }
